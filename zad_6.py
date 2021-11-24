c8e47b5ffb47ef1bfee24e3bf2b9ca4d864bfe9e
katarzynafrelek <katarzyna.frelek@edu.uekat.pl>
2021-11-24 20:32:36 +0100

    skonczone zadania lab 2

--- zad_6.py
+++
@@ -1,9 +0,0 @@
-def podaj_dwie_listy(lista1: list, lista2: list) -> list:
-    zlaczenie_list = list(set(lista1 + lista2))
-    for x in range(len(zlaczenie_list)):
-        zlaczenie_list[x] = zlaczenie_list[x] ** 3
-    return zlaczenie_list
-
-
-result = podaj_dwie_listy([1, 2, 3, 4], [3, 4, 5, 6])
-print(result)
